f67c2c210366d2e685f6e261a9e4e6e7e60720a6
Bryan <bryanecheverrir@gmail.com>
2025-05-29 12:31:20 -0500

    Enhance logging and string manipulation examples in 4.js; update index.html script references

--- IntroduccionJS/css/js/4.js
+++ IntroduccionJS/css/js/4.js
@@ -3,7 +3,19 @@
 const producto = "Monitor";
 const producto2 = String("Televisor");
 const producto3 = new String("Tablet");
+const publicacion = "Albion Online es un MMO RPG de modo sandbox en el que escribes tu propia historia.";
+const correo = "correo@correo.com";
 
-console.log(producto);
+console.log(producto.length);
 console.log(producto2);
-console.log(typeof producto3);
+console.log(producto3);
+
+//IndexOf
+console.log(publicacion.indexOf("MMO")); 
+console.log(producto.indexOf("Monitor")); // Devuelve la posición del primer carácter encontrado
+console.log(producto.indexOf("Tablet")); // Devuelve -1 si no se encuentra
+console.log(correo.indexOf("@")); // Devuelve la posición del carácter '@' en el correo
+
+//Includes (Retorna true o false)
+console.log(publicacion.includes("MMO")); // true
+console.log(producto.includes("Televisor")); // false
