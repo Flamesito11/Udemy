d9c693654b5d90a5d5607a52e70a1aaf7b63310e
Bryan <bryanecheverrir@gmail.com>
2025-05-29 12:12:01 -0500

    Refactor string declarations and logging in 4.js

--- IntroduccionJS/css/js/4.js
+++ IntroduccionJS/css/js/4.js
@@ -1,2 +1,9 @@
-let si = true;
+//Stings o Cadenas de texto
 
+const producto = "Monitor";
+const producto2 = String("Televisor");
+const producto3 = new String("Tablet");
+
+console.log(producto);
+console.log(producto2);
+console.log(typeof producto3);
