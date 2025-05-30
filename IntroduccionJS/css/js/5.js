8434dfab55eadf3dc0b6e2adb8eb3f7c956e6c61
Bryan <bryanecheverrir@gmail.com>
2025-05-29 20:37:23 -0500

    Add arithmetic operations and number declarations in 5.js; update index.html to include 5.js

---
+++ IntroduccionJS/css/js/5.js
@@ -0,0 +1,15 @@
+//numeros
+
+const numero1 = 200;
+const numero2 = 100;
+const numero3 = 174.9;
+const numero4 = .12;
+const numero5 = -9;
+
+
+//Operadores Aritméticos
+console.log(numero1 + numero2); // Suma
+console.log(numero1 - numero2); // Resta
+console.log(numero1 * numero2); // Multiplicación   
+console.log(numero1 / numero2); // División
+console.log(numero1 % numero2); // Módulo (Resto de la división)
