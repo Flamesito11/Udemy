6227caf9aaeaf55062e131925283e556bfdb4559
Bryan <bryanecheverrir@gmail.com>
2025-06-01 19:54:53 -0500

    Add additional Math object examples in 6.js

--- IntroduccionJS/css/js/6.js
+++ IntroduccionJS/css/js/6.js
@@ -4,6 +4,14 @@ let resultado;
 
 resultado = Math.PI; // Valor de PI
 resultado = Math.round(2.4); // Redondea al entero más cercano
+resultado = Math.ceil(2.4); // Redondea hacia arriba
+resultado = Math.floor(2.4); // Redondea hacia abajo
+resultado = Math.sqrt(64); // Raíz cuadrada
+resultado = Math.abs(-200); // Valor absoluto
+resultado = Math.min(3, 5, 1, 8, -2); // Valor mínimo
+resultado = Math.max(3, 5, 1, 8, -2); // Valor máximo
+resultado = Math.random(); // Número aleatorio entre 0 y 1
+resultado = Math.floor(Math.random() * 10);
 
 
 console.log(resultado);
