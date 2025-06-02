3e3e2f68635f499bcfc3da3b0ec42e9f58e00475
Bryan <bryanecheverrir@gmail.com>
2025-06-02 18:51:50 -0500

    Add 10.js with object examples; update index.html to reference 10.js

---
+++ IntroduccionJS/css/js/10.js
@@ -0,0 +1,21 @@
+// Objetos
+
+const nombre_Producto = "Monitor";
+const precio_Producto = 1000;
+const disponible = true;
+
+
+// Definición de un objeto
+const producto = {
+    nombre: "Monitor",
+    precio: 1000,
+    disponible: true
+}
+
+console.log(producto);
+
+console.log(producto.nombre); // Acceso a una propiedad del objeto
+console.log(producto.precio); // Acceso a otra propiedad del objeto
+console.log(producto.disponible); // Acceso a otra propiedad del objeto
+
+consoconsole.log(producto["nombre"]); // Acceso a una propiedad del objeto usando notación de corchetes
