9c410cc56c2a23ac7dd838bb58322ad021d4d95a
Bryan <bryanecheverrir@gmail.com>
2025-06-01 20:06:04 -0500

    Add new operations and increment examples in 7.js; update index.html to reference 7.js

---
+++ IntroduccionJS/css/js/7.js
@@ -0,0 +1,22 @@
+// Orden de las operaciones
+
+let resultado;
+
+resultado = 20 + 30 * 2; // Multiplicación antes que suma
+
+resultado = (100 + 400+ 300) * .2;
+
+// console.log(resultado);
+
+// Incrementos
+
+let puntaje = 0;
+
+// Incremento en 1 ++
+
+puntaje++;
+
+//decremento en 1 --
+puntaje--;
+
+console.log(puntaje); 
