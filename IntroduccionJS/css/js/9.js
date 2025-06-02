4e61ea67a69bbb9b6cbd37eedd967ea9d91f7cc6
Bryan <bryanecheverrir@gmail.com>
2025-06-02 18:43:29 -0500

    Add 9.js with boolean examples; update index.html to reference 9.js

---
+++ IntroduccionJS/css/js/9.js
@@ -0,0 +1,9 @@
+// Booleanos
+
+const booleano1 = true;
+const booleano2 = false;
+const booleano3 = "true"; // "true" es un string, no un booleano
+
+console.log(booleano1); 
+console.log(booleano2);
+console.log(booleano3); // "true" es un string, no un booleano
