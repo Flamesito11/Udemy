c19137a8b2aa01bfd7e9d302e3ec27b7b9e693b7
Bryan <bryanecheverrir@gmail.com>
2025-06-01 20:17:41 -0500

    Add 8.js with concatenation and template string examples; update index.html to reference 8.js

---
+++ IntroduccionJS/css/js/8.js
@@ -0,0 +1,8 @@
+const nombre = 'Bryan';
+const email = 'Nose@gmail.com';
+
+// Concatenación
+console.log("Nombre del Cliente: " + nombre + " Email: " + email);
+
+//Tempalte Strings
+console.log(`Nombre Cliente: ${nombre} Email: ${email}`);
