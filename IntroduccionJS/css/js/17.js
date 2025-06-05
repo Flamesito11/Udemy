9342bb509ab20c6b38851637d8d9f6068fc0cdf5
Bryan <bryanecheverrir@gmail.com>
2025-06-05 17:54:45 -0500

    Add 17.js with function and method examples; update index.html to reference 17.js

---
+++ IntroduccionJS/css/js/17.js
@@ -0,0 +1,7 @@
+//Diferencia entre metodo y funcion
+
+const numero1 = 50;
+const numero2 = "90";
+
+console.log( parseInt(numero2) );// ParseInt() es una funcion que convierte un string a un numero entero
+console.log( numero1.toString() );// .Tostring() es un metodo que convierte un numero a un string
