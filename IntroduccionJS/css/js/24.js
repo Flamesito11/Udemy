b9f2f50415a9a9dcd75bb2410df0e83bf31cdee1
Bryan <bryanecheverrir@gmail.com>
2025-08-24 12:18:09 -0500

    Update index.html to reference 24.js and restore commented-out code in 24.js

--- IntroduccionJS/css/js/24.js
+++ IntroduccionJS/css/js/24.js
@@ -13,16 +13,43 @@
 //     }
 // }
 
-const carrito = [
-    { nombre: "Monitor HD", precio: 700 },
-    { nombre: "Televisor Smart TV", precio: 1000 },
-    { nombre: "Tablet", precio: 450 },
-    { nombre: "Audifonos", precio: 100 },
-    { nombre: "Teclado", precio: 300 },
-    { nombre: "Celular", precio: 700 }
+// // const carrito = [
+// //     { nombre: "Monitor HD", precio: 700 },
+// //     { nombre: "Televisor Smart TV", precio: 1000 },
+// //     { nombre: "Tablet", precio: 450 },
+// //     { nombre: "Audifonos", precio: 100 },
+// //     { nombre: "Teclado", precio: 300 },
+// //     { nombre: "Celular", precio: 700 }
 
-];
+// // ];
 
-for (let i = 0; i < carrito.length; i++) {
-    console.log(carrito[i].nombre);
-}
+// // for (let i = 0; i < carrito.length; i++) {
+// //     console.log(carrito[i].nombre);
+// // }
+
+
+//Ciclo while
+
+// let i = 0;
+
+// while (i <= 100) {// condicion
+    
+//     if(i % 2 == 0) {
+//         console.log(i + " es par")
+//     }
+//     else{
+//         console.log(i + " es impar")
+//     }
+
+//     i++;//incremento
+// }
+
+
+// Ciclo do while
+
+let i = 0;
+
+do{
+    console.log(i);
+    i++;
+}while(i < 10);
